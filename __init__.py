6fdff2f5e09a4ada779fd51d4096e88ccf602968
adriana3365 <a.kengnemola@ecoles-epsi.net>
2025-04-07 11:01:02 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
-    pyramide = '<pre>'
-    for i in range(1, valeur + 1):
-        espaces = ' ' * (valeur - i)
-        chiffres = ''.join(str(j) for j in range(1, i + 1)) + ''.join(str(j) for j in range(i - 1, 0, -1))
-        pyramide += espaces + chiffres + '\n'
-    pyramide += '</pre>'
-    return pyramide
+    a, b = 0, 1
+    sequence = [a, b]
+    for _ in range(2, valeur):
+        a, b = b, a + b
+        sequence.append(b)
+    
+    fibonacci = '<pre>' + ', '.join(map(str, sequence)) + '</pre>'
+    return fibonacci
 
 if __name__ == "__main__":
     app.run(debug=True)
- 
